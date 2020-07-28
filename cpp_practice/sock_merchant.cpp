a34e4d365613cf5bbc5de1ae71963ef752b47b7e
Bridget Murphy <39529905+brmu8861@users.noreply.github.com>
2020-07-28 09:40:52 -0700

    Add files via upload
    Add cpp_practice folder and sock_merchant

---
+++ cpp_practice/sock_merchant.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+std::vector<string> split_string(string);
+
+// Complete the sockMerchant function below.
+int sockMerchant(int n, std::vector<int> ar) {
+    std::sort(ar.begin(), ar.end());
+
+    for(int i=0; i<ar.size(); ++i){
+        std::cout << ar[i] << ' ';
+    }
+
+    int matching_socks = 0;
+    for(int i=0; i<ar.size(); ++i){
+        cout << i << endl;
+        cout << ar[i] << endl;
+        if (ar[i] == ar[i+1]){
+            matching_socks++;
+            if(ar[i] == ar[i+2]){
+                i = i + 1;
+            }
+        }
+        else{
+            matching_socks = matching_socks;
+        }
+    }
+    return matching_socks;
+}
